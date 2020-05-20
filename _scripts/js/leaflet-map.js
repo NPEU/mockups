8efcc48b0c62846dff87fa57558435a40712753a
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2020-05-20 09:17:14 +0100

    Update Leaflet/mapbox.

--- _scripts/js/leaflet-map.js
+++ _scripts/js/leaflet-map.js
@@ -49,11 +49,24 @@ function leafletMapInitialize(map_container_id, map_data, markers) {
         }
     });
 
+    /*
     // Mapbox tiles (https://www.mapbox.com/)
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
+        accessToken: map_data.token
+    }).addTo(map);
+    */
+    
+    
+    // Mapbox tiles (https://www.mapbox.com/)
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
         accessToken: map_data.token
     }).addTo(map);
 
